64d9c1be0c764fb1521012ef6be24e391a6ea964
mengyun3 <mengyun3@staff.weibo.com>
2023-09-25 22:36:54 +0800

    first

---
+++ src/main/java/com/weibo/weivectorsearch/WeiVectorSearch.java
@@ -0,0 +1,11 @@
+package com.weibo.weivectorsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiVectorSearch {
+    public static void main(String[] args) {
+        SpringApplication.run(WeiVectorSearch.class, args);
+    }
+}

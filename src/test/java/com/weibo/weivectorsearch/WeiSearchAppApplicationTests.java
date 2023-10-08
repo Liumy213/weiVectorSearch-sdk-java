c0cd04ae00e8918a5e5bffd1418094b1a9eed6ae
mengyun3 <mengyun3@staff.weibo.com>
2023-10-08 19:14:33 +0800

    v0.0.1

---
+++ src/test/java/com/weibo/weivectorsearch/WeiSearchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weibo.weivectorsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiSearchAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

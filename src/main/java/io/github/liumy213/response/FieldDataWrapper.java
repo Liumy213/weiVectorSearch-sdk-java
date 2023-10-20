8e3a12803483ea280d5de3b5ed87afa535af21bd
mengyun3 <mengyun3@staff.weibo.com>
2023-10-20 14:36:03 +0800

    v0.0.5

---
+++ src/main/java/io/github/liumy213/response/FieldDataWrapper.java
@@ -0,0 +1,229 @@
+package io.github.liumy213.response;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.ProtocolStringList;
+import io.github.liumy213.exception.IllegalResponseException;
+import io.github.liumy213.exception.ParamException;
+import io.github.liumy213.rpc.DataType;
+import io.github.liumy213.rpc.FieldData;
+import lombok.NonNull;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static io.github.liumy213.rpc.DataType.JSON;
+
+
+/**
+ * Utility class to wrap response of <code>query/search</code> interface.
+ */
+public class FieldDataWrapper {
+    private final FieldData fieldData;
+
+    public FieldDataWrapper(@NonNull FieldData fieldData) {
+        this.fieldData = fieldData;
+    }
+
+    public boolean isVectorField() {
+        return fieldData.getType() == DataType.FloatVector || fieldData.getType() == DataType.BinaryVector;
+    }
+
+    public boolean isJsonField() {
+        return fieldData.getType() == JSON;
+    }
+
+    public boolean isDynamicField() {
+        return fieldData.getType() == JSON && fieldData.getIsDynamic();
+    }
+
+    /**
+     * Gets the dimension value of a vector field.
+     * Throw {@link IllegalResponseException} if the field is not a vector filed.
+     *
+     * @return <code>int</code> dimension of the vector field
+     */
+    public int getDim() throws IllegalResponseException {
+        if (!isVectorField()) {
+            throw new IllegalResponseException("Not a vector field");
+        }
+        return (int) fieldData.getVectors().getDim();
+    }
+
+    /**
+     * Gets the row count of a field.
+     * * Throws {@link IllegalResponseException} if the field type is illegal.
+     *
+     * @return <code>long</code> row count of the field
+     */
+    public long getRowCount() throws IllegalResponseException {
+        DataType dt = fieldData.getType();
+        switch (dt) {
+            case FloatVector: {
+                int dim = getDim();
+                List<Float> data = fieldData.getVectors().getFloatVector().getDataList();
+                if (data.size() % dim != 0) {
+                    throw new IllegalResponseException("Returned float vector field data array size doesn't match dimension");
+                }
+
+                return data.size()/dim;
+            }
+            case BinaryVector: {
+                int dim = getDim();
+                ByteString data = fieldData.getVectors().getBinaryVector();
+                if ((data.size()*8) % dim != 0) {
+                    throw new IllegalResponseException("Returned binary vector field data array size doesn't match dimension");
+                }
+
+                return (data.size()*8)/dim;
+            }
+            case Int64:
+                return fieldData.getScalars().getLongData().getDataList().size();
+            case Int32:
+            case Int16:
+            case Int8:
+                return fieldData.getScalars().getIntData().getDataList().size();
+            case Bool:
+                return fieldData.getScalars().getBoolData().getDataList().size();
+            case Float:
+                return fieldData.getScalars().getFloatData().getDataList().size();
+            case Double:
+                return fieldData.getScalars().getDoubleData().getDataList().size();
+            case VarChar:
+            case String:
+                return fieldData.getScalars().getStringData().getDataList().size();
+            case JSON:
+                return fieldData.getScalars().getJsonData().getDataList().size();
+            default:
+                throw new IllegalResponseException("Unsupported data type returned by FieldData");
+        }
+    }
+
+    /**
+     * Returns the field data according to its type:
+     *      float vector field return List of List Float,
+     *      binary vector field return List of ByteBuffer
+     *      int64 field return List of Long
+     *      int32/int16/int8 field return List of Integer
+     *      boolean field return List of Boolean
+     *      float field return List of Float
+     *      double field return List of Double
+     *      varchar field return List of String
+     *      etc.
+     *
+     * Throws {@link IllegalResponseException} if the field type is illegal.
+     *
+     * @return <code>List</code>
+     */
+    public List<?> getFieldData() throws IllegalResponseException {
+        DataType dt = fieldData.getType();
+        switch (dt) {
+            case FloatVector: {
+                int dim = getDim();
+                List<Float> data = fieldData.getVectors().getFloatVector().getDataList();
+                if (data.size() % dim != 0) {
+                    throw new IllegalResponseException("Returned float vector field data array size doesn't match dimension");
+                }
+
+                List<List<Float>> packData = new ArrayList<>();
+                int count = data.size() / dim;
+                for (int i = 0; i < count; ++i) {
+                    packData.add(data.subList(i * dim, (i + 1) * dim));
+                }
+                return packData;
+            }
+            case BinaryVector: {
+                int dim = getDim();
+                ByteString data = fieldData.getVectors().getBinaryVector();
+                if ((data.size()*8) % dim != 0) {
+                    throw new IllegalResponseException("Returned binary vector field data array size doesn't match dimension");
+                }
+
+                List<ByteBuffer> packData = new ArrayList<>();
+                int bytePerVec = dim/8;
+                int count = data.size()/bytePerVec;
+                for (int i = 0; i < count; ++i) {
+                    ByteBuffer bf = ByteBuffer.allocate(bytePerVec);
+                    bf.put(data.substring(i * bytePerVec, (i + 1) * bytePerVec).toByteArray());
+                    packData.add(bf);
+                }
+                return packData;
+            }
+            case Int64:
+                return fieldData.getScalars().getLongData().getDataList();
+            case Int32:
+            case Int16:
+            case Int8:
+                return fieldData.getScalars().getIntData().getDataList();
+            case Bool:
+                return fieldData.getScalars().getBoolData().getDataList();
+            case Float:
+                return fieldData.getScalars().getFloatData().getDataList();
+            case Double:
+                return fieldData.getScalars().getDoubleData().getDataList();
+            case VarChar:
+            case String:
+                ProtocolStringList protoStrList = fieldData.getScalars().getStringData().getDataList();
+                return protoStrList.subList(0, protoStrList.size());
+            case JSON:
+                List<ByteString> dataList = fieldData.getScalars().getJsonData().getDataList();
+                return dataList.stream().map(ByteString::toByteArray).collect(Collectors.toList());
+            default:
+                throw new IllegalResponseException("Unsupported data type returned by FieldData");
+        }
+    }
+
+    public Integer getAsInt(int index, String paramName) throws IllegalResponseException {
+        if (isJsonField()) {
+            String result = getAsString(index, paramName);
+            return result == null ? null : Integer.parseInt(result);
+        }
+        throw new IllegalResponseException("Only JSON type support this operation");
+    }
+
+    public String getAsString(int index, String paramName) throws IllegalResponseException {
+        if (isJsonField()) {
+            JSONObject jsonObject = parseObjectData(index);
+            return jsonObject.getString(paramName);
+        }
+        throw new IllegalResponseException("Only JSON type support this operation");
+    }
+
+    public Boolean getAsBool(int index, String paramName) throws IllegalResponseException {
+        if (isJsonField()) {
+            String result = getAsString(index, paramName);
+            return result == null ? null : Boolean.parseBoolean(result);
+        }
+        throw new IllegalResponseException("Only JSON type support this operation");
+    }
+
+    public Double getAsDouble(int index, String paramName) throws IllegalResponseException {
+        if (isJsonField()) {
+            String result = getAsString(index, paramName);
+            return result == null ? null : Double.parseDouble(result);
+        }
+        throw new IllegalResponseException("Only JSON type support this operation");
+    }
+
+    public Object get(int index, String paramName) throws IllegalResponseException {
+        if (isJsonField()) {
+            JSONObject jsonObject = parseObjectData(index);
+            return jsonObject.get(paramName);
+        }
+        throw new IllegalResponseException("Only JSON type support this operation");
+    }
+
+    public Object valueByIdx(int index) throws ParamException {
+        if (index < 0 || index >= getFieldData().size()) {
+            throw new ParamException("index out of range");
+        }
+        return getFieldData().get(index);
+    }
+
+    private JSONObject parseObjectData(int index) {
+        Object object = valueByIdx(index);
+        return JSONObject.parseObject(new String((byte[])object));
+    }
+}
